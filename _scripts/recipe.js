9af898c1cd7c66dccc767df442bb8ded4a5aac3b
Evan Williams <evan@williams.ee>
2016-12-01 11:26:05 +0000

    Auto-hide recipe substitutions

---
+++ _scripts/recipe.js
@@ -0,0 +1,21 @@
+---
+---
+function toggleState(state){
+  if(state === "none") {
+    return "inline-flex";
+  }
+  if(state == "inline-flex"){
+    return "none";
+  }
+}
+
+$(document).ready(function(){
+  $(".recipe-ingredient-substitutions").css("display","none");
+
+  $(".recipe-ingredient-dangers").on("click", function() {
+    target = $(this).siblings(".recipe-ingredient-substitutions");
+    state = target.css("display");
+    state = toggleState(state);
+    target.css("display",state);
+  })
+})
