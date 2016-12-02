1d18833b2f474a54b78d0a308a3cd2f0979dcac7
Evan Williams <evan@williams.ee>
2016-12-02 18:05:59 +0000

    Built search database

--- _scripts/libraries/tipue/tipuesearch_content.js
+++ _scripts/libraries/tipue/tipuesearch_content.js
@@ -1,16 +1,77 @@
 ---
 ---
 var tipuesearch = {"pages": [
-     {"title": "Tipue", "text": "", "tags": "jQuery HTML5 CSS", "url": "http://www.tipue.com"},
-     {"title": "Tipue Search, a site search engine jQuery plugin", "text": "Tipue Search is a site search engine jQuery plugin. It's free, open source, responsive and fast. Tipue Search only needs a browser that supports jQuery. It doesn't need MySQL or similar. In Static mode it doesn't even need a web server.", "tags": "JavaScript", "url": "http://www.tipue.com/search"},
-     {"title": "Tipue Search Documentation", "text": "Tipue Search is a site search engine jQuery plugin. It's free, open source and responsive. Tipue Search uses various modes for loading content. Static mode uses a JavaScript object, while JSON mode uses JSON. Live mode grabs content from a list of pages dynamically.", "tags": "docs", "url": "http://www.tipue.com/search/docs"},
-     {"title": "Tipue Search Static mode demo", "text": "Tipue Search is a site search engine jQuery plugin. This is a demo of Static mode. Enter tipue into the search box above.", "tags": "", "url": "http://www.tipue.com/search/demos/static"},
-     {"title": "Tipue Search Live mode demo", "text": "Tipue Search is a site search engine jQuery plugin. This is a demo of Live mode. Enter tipue into the search box above.", "tags": "", "url": "http://www.tipue.com/search/demos/live"},
-     {"title": "Tipue jQuery plugins Support", "text": "If you're stuck we offer a range of flexible support plans for our jQuery plugins.", "tags": "", "url": "http://www.tipue.com/support"},
-     {"title": "Tipr, a small and simple jQuery tooltip plugin", "text": "Tipr is a small and simple jQuery tooltip plugin. It's free and open source. Tipr displays attractive tooltips, and it's a shade under 4KB, CSS included.", "tags": "JavaScript", "url": "http://www.tipue.com/tipr"},
-     {"title": "The Tipue blog", "text": "An occasional blog covering CSS, web development, etc.", "tags": "HTML5", "url": "http://www.tipue.com/?d=2"},
-     {"title": "About Tipue", "text": "Tipue is a small web development studio based in North London. We've been around for well over a decade. We design innovative add-ins, plugins, code and features with heavy-duty Perl, MySQL and jQuery.", "tags": "", "url": "http://www.tipue.com/about"},
-     {"title": "The Tipue blog - The complete guide to centering a div", "text": "Every new developer inevitably finds that centering a div isn't as obvious as you'd expect. Centering what's inside a div is easy enough by giving the text-align property a value of center, but then things tend to get a bit sticky. When you get to centering a div vertically, you can end up in a world of CSS hurt.", "tags": "HTML", "url": "http://www.tipue.com/blog/center-a-div"},
-     {"title": "The Tipue blog - Native HTML5 autocomplete with input list", "text": "This article shows how with the HTML5 input list attribute and datalist element you can easily set up an input box with a custom autocomplete without jQuery, JavaScript or similar.", "tags": "", "url": "http://www.tipue.com/blog/input-list"},
-     {"title": "The Tipue blog - The really simple guide to z-index", "text": "The CSS z-index property often trips up new and even experienced developers. The aim of this article is to boil down a somewhat-complex specification to 3 major points, which should ease most z-index pain.", "tags": "", "url": "http://www.tipue.com/z-index"}
+  {% for author in site.authors %}
+    {
+      "title" : "{{author.name}}",
+      "text" : `{{author.content | strip_html | strip_new_lines}}`,
+      "url" : "{{author.url}}",
+      "tags" : "author"
+    },
+  {% endfor %}
+  {% for party in site.parties %}
+    {
+      "title" : "{{party.title}}",
+      "text" : `{{party.content | strip_html | strip_new_lines}}`,
+      "url" : "{{party.url}}",
+      "tags" : "party"
+    },
+  {% endfor %}
+  {% for page in site.pages %}
+    {
+      "title" : "{{post.title}}",
+      "text" : `{{ page.content | strip_html | strip_new_lines }}`,
+      "url" : "{{post.url}}",
+      "tags" : ""
+    }
+  {% endfor %}
+  {% for post in site.posts %}
+    {
+      "title" : "{{post.title}}",
+      "text" : `{{post.content | strip_html | strip_new_lines}}`,
+      "url" : "{{post.url}}",
+
+      {% assign tags = post.tags %}
+      {% comment %}
+        Build a list of the dietry requirements the recipe follows
+      {% endcomment %}
+      {% if post.recipe %}
+        {% assign requirements = site.data.Dietry-Requirements.dietry-requirements %}
+        {% assign tags = tags | push: "recipe" %}
+
+        {% assign restrictions = "" | split: "" %}
+        {% for ingredient in page.recipe.ingredients %}
+          {% assign ingredient-detail = site.data.Ingredients[ingredient.item] %}
+          {% if ingredient-detail.dietry-requirements %}
+            {% for restriction in ingredient-detail.dietry-requirements %}
+              {% assign restrictions = restrictions | push: restriction %}
+            {% endfor %}
+          {% endif %}
+        {% endfor %}
+
+        {% assign restrictions = restrictions | uniq %}
+
+        {% for requirement in requirements %}
+        {% assign isSafe = true %}
+        {% for restriction in restrictions %}
+          {% if requirement[0] == restriction %}
+            {% assign isSafe = false %}
+            {% break %}
+          {% endif %}
+        {% endfor %}
+
+        {% if isSafe %}
+          {% assign tags = tags | push: requirement[1].safe %}
+        {% endif %}
+        {% endfor%}
+      {% endif %}
+      "tags" : "{{ tags | join: ','}}"
+    },
+  {% endfor %}
+  {
+    "title" : "",
+    "text" : "",
+    "url" : "",
+    "tags" : ""
+  }
 ]};
