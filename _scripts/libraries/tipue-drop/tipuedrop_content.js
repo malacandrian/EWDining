119c13430f28da0d8f9e7de8c86c0a23722bbfd3
Evan Williams <evan@williams.ee>
2016-12-02 21:10:40 +0000

    Built autocomplete database

--- _scripts/libraries/tipue-drop/tipuedrop_content.js
+++ _scripts/libraries/tipue-drop/tipuedrop_content.js
@@ -1,17 +1,23 @@
 ---
 ---
-var tipuedrop = {"pages": [
-     {"title": "Tipue Search", "thumb": "img/search.png", "text": "a site search engine jQuery plugin", "url": "http://www.tipue.com/search"},
-     {"title": "Tipue drop", "thumb": "img/drop.png", "text": "a search suggestion box jQuery plugin", "url": "http://www.tipue.com/drop"},
-     {"title": "Tipr", "thumb": "img/tipr.png", "text": "a small and simple tooltip jQuery plugin", "url": "http://www.tipue.com/tipr"}
-]};
-
 var tipuedrop = {"pages" : [
+
+  {% for party in site.parties %}
+    {% if party.inSearch %}
+      {
+        "title" : "{{party.title}}",
+        "thumb" : "{{party.icon-root}}{{party.icon-path}}",
+        "text" : `{{party.content | strip_html}}`,
+        "url" : "{{party.url}}"
+      },
+    {% endif %}
+  {% endfor %}
+  
   {% for author in site.authors %}
     {% if author.inSearch %}
       {
         "title" : "{{author.name}}",
-        "thumb" : "{{author.picture-root}}{{include.author.picture}}",
+        "thumb" : "{{author.picture-root}}{{author.picture}}",
         "text" : `{{author.content | strip_html }}`,
         "url" : "{{author.url}}"
       },
